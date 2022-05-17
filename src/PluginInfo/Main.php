4ab1580c8e1a68ed960dc5f26c94aab996006a87
Estem0 <101932306+Estem01@users.noreply.github.com>
2022-05-17 02:43:17 -0300

    Create Main.php

---
+++ src/PluginInfo/Main.php
@@ -0,0 +1,22 @@
+<?php
+
+//ALL THE RIGHTS RESERVED BY ESTEM0
+
+namespace PluginInfo
+
+use pocketmine\PluginBase;
+
+use pocketmine\Player;
+
+use pocketmine\Server;
+
+use pocketmine\command\Command;
+
+use pocketmine\command\CommandSender;
+
+class Main extends PluginBase implements Listener{
+
+  public function(){ 
+  $this->getLogger->info('InfoBasic Ligado') }
+
+}
